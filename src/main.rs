d358e9dd9ffe0e7c2ece9cf626844a2e366e9fc8
Teajey <21069848+Teajey@users.noreply.github.com>
2023-10-12 01:01:35 +1300

    feat: update data after fs events

--- src/main.rs
+++ src/main.rs
@@ -3,36 +3,170 @@ use std::{
     path::{Path, PathBuf},
 };
 
-use anyhow::{anyhow, Result};
+use anyhow::Result;
 use notify::{RecursiveMode, Watcher};
 
-fn main() {
-    if let Err(err) = run() {
-        eprintln!("Error: {err}");
-        std::process::exit(1);
-    }
-}
-
 #[derive(Debug)]
-struct MarkdownFile {
+struct MarkdownData {
     frontmatter: Option<serde_yaml::Mapping>,
     body: String,
 }
 
-fn markdown_filepaths(dir: &Path) -> Result<Vec<PathBuf>> {
-    let mut files = vec![];
-    for f in std::fs::read_dir(dir)? {
-        let p = f?.path();
-        if let Some(ext) = p.extension() {
-            let str_ext = ext
-                .to_str()
-                .ok_or_else(|| anyhow!("Couldn't parse file extension as UTF-8 string"))?;
-            if str_ext == "md" {
-                files.push(p);
-            }
+impl MarkdownData {
+    fn from_string(string: String) -> Result<Self, serde_yaml::Error> {
+        if !string.starts_with("---\n") {
+            let md = MarkdownData {
+                frontmatter: None,
+                body: string,
+            };
+            return Ok(md);
         }
+
+        let [_, frontmatter, body] = string.splitn(3, "---\n").collect::<Vec<_>>()[..] else {
+            let md = MarkdownData {
+                frontmatter: None,
+                body: string,
+            };
+            return Ok(md);
+        };
+
+        let frontmatter = serde_yaml::from_str(frontmatter)?;
+
+        Ok(MarkdownData {
+            frontmatter: Some(frontmatter),
+            body: body.to_owned(),
+        })
     }
-    Ok(files)
+}
+
+fn path_is_markdown(path: &Path) -> bool {
+    path.extension()
+        .and_then(std::ffi::OsStr::to_str)
+        .is_some_and(|ext| ext == "md")
+}
+
+fn markdown_filepaths(dir: &Path) -> Result<Vec<PathBuf>, std::io::Error> {
+    std::fs::read_dir(dir)?
+        .filter_map(|entry| {
+            entry
+                .map(|entry| {
+                    let path = entry.path();
+                    if path_is_markdown(&path) {
+                        Some(path)
+                    } else {
+                        None
+                    }
+                })
+                .transpose()
+        })
+        .collect()
+}
+
+struct DataSynchroniser {
+    data: HashMap<PathBuf, MarkdownData>,
+}
+
+impl notify::EventHandler for DataSynchroniser {
+    fn handle_event(&mut self, event: notify::Result<notify::Event>) {
+        match event {
+            Ok(notify::Event {
+                kind,
+                paths,
+                attrs: _,
+            }) => {
+                let path = paths.first().expect("event must have at least one path");
+                if !path_is_markdown(path) {
+                    return;
+                }
+                match kind {
+                    notify::EventKind::Modify(notify::event::ModifyKind::Name(
+                        notify::event::RenameMode::Any,
+                    )) => {
+                        let was_removed = self.data.remove(path).is_some();
+                        if !was_removed {
+                            let new_content = match std::fs::read_to_string(path) {
+                                Ok(content) => content,
+                                Err(err) => {
+                                    eprintln!(
+                                        "Failed to read file ({path:?}) after a Rename event: {err}"
+                                    );
+                                    return;
+                                }
+                            };
+                            let new_data = match MarkdownData::from_string(new_content) {
+                                Ok(new_data) => new_data,
+                                Err(err) => {
+                                    eprintln!("Couldn't read markdown during Create event: {err}");
+                                    return;
+                                }
+                            };
+                            self.data.insert(path.clone(), new_data);
+                        }
+                    }
+                    notify::EventKind::Modify(notify::event::ModifyKind::Data(
+                        notify::event::DataChange::Content,
+                    )) => {
+                        let new_content = match std::fs::read_to_string(path) {
+                            Ok(content) => content,
+                            Err(err) => {
+                                eprintln!(
+                                    "Failed to read file ({path:?}) after a Modify event: {err}"
+                                );
+                                return;
+                            }
+                        };
+                        let Some(data) = self.data.get_mut(path) else {
+                            eprintln!("Tried to get data for a path ({path:?}) that doesn't exist in memory.");
+                            return;
+                        };
+                        let new_data = match MarkdownData::from_string(new_content) {
+                            Ok(new_data) => new_data,
+                            Err(err) => {
+                                eprintln!("Couldn't read markdown during Modify event: {err}");
+                                return;
+                            }
+                        };
+                        *data = new_data;
+                    }
+                    notify::EventKind::Remove(notify::event::RemoveKind::File) => {
+                        let was_removed = self.data.remove(path).is_some();
+                        if !was_removed {
+                            eprintln!("Recieved a removal event for a path ({path:?}) that didn't exist in memory.");
+                        }
+                    }
+                    notify::EventKind::Create(notify::event::CreateKind::File) => {
+                        let new_content = match std::fs::read_to_string(path) {
+                            Ok(content) => content,
+                            Err(err) => {
+                                eprintln!(
+                                    "Failed to read file ({path:?}) after a Create event: {err}"
+                                );
+                                return;
+                            }
+                        };
+                        if self.data.contains_key(path) {
+                            eprintln!(
+                                "A Create event occurred for a path ({path:?}) but it already exists in memory."
+                            );
+                            return;
+                        }
+                        let new_data = match MarkdownData::from_string(new_content) {
+                            Ok(new_data) => new_data,
+                            Err(err) => {
+                                eprintln!("Couldn't read markdown during Create event: {err}");
+                                return;
+                            }
+                        };
+                        self.data.insert(path.clone(), new_data);
+                    }
+                    event => println!("watch event: {event:?}"),
+                }
+            }
+            Err(e) => println!("watch error: {e:?}"),
+        }
+
+        println!("\nUpdated data:\n{:#?}", self.data);
+    }
 }
 
 fn run() -> Result<()> {
@@ -45,47 +179,33 @@ fn run() -> Result<()> {
     let markdown_files = markdown_fps
         .into_iter()
         .map(|path| {
-            let md = std::fs::read_to_string(&path)?;
+            let string = std::fs::read_to_string(&path)?;
 
-            if !md.starts_with("---\n") {
-                let md = MarkdownFile {
-                    frontmatter: None,
-                    body: md,
-                };
-                return Ok((path, md));
-            }
-
-            let [_, frontmatter, body] = md.splitn(3, "---\n").collect::<Vec<_>>()[..] else {
-                let md = MarkdownFile {
-                    frontmatter: None,
-                    body: md,
-                };
-                return Ok((path, md));
-            };
-            dbg!(frontmatter, body);
-
-            let frontmatter = serde_yaml::from_str(frontmatter)?;
-
-            let md = MarkdownFile {
-                frontmatter: Some(frontmatter),
-                body: body.to_owned(),
-            };
+            let md = MarkdownData::from_string(string)?;
 
             Ok((path, md))
         })
         .collect::<Result<HashMap<_, _>>>()?;
 
-    dbg!(markdown_files);
+    let synchroniser = DataSynchroniser {
+        data: markdown_files,
+    };
 
     // Automatically select the best implementation for your platform.
-    let mut watcher = notify::recommended_watcher(|res| match res {
-        Ok(event) => println!("event: {event:?}"),
-        Err(e) => println!("watch error: {e:?}"),
-    })?;
+    let mut watcher = notify::recommended_watcher(synchroniser)?;
 
     // Add a path to be watched. All files and directories at that path and
     // below will be monitored for changes.
     watcher.watch(&current_dir, RecursiveMode::NonRecursive)?;
 
+    std::thread::park();
+
     Ok(())
+}
+
+fn main() {
+    if let Err(err) = run() {
+        eprintln!("Error: {err}");
+        std::process::exit(1);
+    }
 }
