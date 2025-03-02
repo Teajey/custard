07e8ef45fb79edb70b40470ca9e3bcba581c0ae6
Teajey <21069848+Teajey@users.noreply.github.com>
2025-03-02 18:32:50 +0900

    feat: single query support

--- go/client/sock/client.go
+++ go/client/sock/client.go
@@ -23,6 +23,14 @@ type GetRequest struct {
 	OrderDesc bool   `msgpack:"order_desc,omitempty"`
 }
 
+type QueryRequest struct {
+	Name      string         `msgpack:"name"`
+	Query     map[string]any `msgpack:"query"`
+	SortKey   string         `msgpack:"sort_key,omitempty"`
+	OrderDesc bool           `msgpack:"order_desc,omitempty"`
+	Intersect bool           `msgpack:"intersect"`
+}
+
 type getResponse struct {
 	File         any    `msgpack:"file"`
 	PrevFileName string `msgpack:"prev_file_name"`
@@ -39,6 +47,44 @@ func SendSingleGetRequest(socketPath string, getReq GetRequest) (*getResponse, e
 	req := taggedRequest{
 		Tag:   "SingleGet",
 		Value: getReq,
+	}
+
+	enc := msgpack.NewEncoder(conn)
+	if err := enc.Encode(req); err != nil {
+		return nil, fmt.Errorf("Failed to encode request: %w", err)
+	}
+
+	var resp *taggedResponse
+	dec := msgpack.NewDecoder(conn)
+	if err := dec.Decode(&resp); err != nil {
+		return nil, fmt.Errorf("Failed to decode response: %w", err)
+	}
+
+	switch resp.Tag {
+	case "Ok":
+		var getResp getResponse
+		err := msgpack.Unmarshal(resp.Value, &getResp)
+		if err != nil {
+			return nil, fmt.Errorf("Could not unmarshal response value: %w", err)
+		}
+		return &getResp, nil
+	case "InternalServerError":
+		return nil, fmt.Errorf("Custard had internal server error")
+	default:
+		return nil, fmt.Errorf("Unrecognised tag from server: %s", resp.Tag)
+	}
+}
+
+func SendSingleQueryRequest(socketPath string, queryReq QueryRequest) (*getResponse, error) {
+	conn, err := net.Dial("unix", socketPath)
+	if err != nil {
+		return nil, fmt.Errorf("Failed to dial: %w", err)
+	}
+	defer conn.Close()
+
+	req := taggedRequest{
+		Tag:   "SingleQuery",
+		Value: queryReq,
 	}
 
 	enc := msgpack.NewEncoder(conn)
--- go/client/sock/test/client_test.go
+++ go/client/sock/test/client_test.go
@@ -29,3 +29,31 @@ func TestSendSingleGetRequest(t *testing.T) {
 		t.Logf("Next file name not '%s'. Found: %s", expectedNextFileName, resp.NextFileName)
 	}
 }
+
+func TestSendSingleQueryRequest(t *testing.T) {
+	resp, err := sock.SendSingleQueryRequest("/tmp/custard", sock.QueryRequest{
+		Name: "chai-cheese.md",
+		Query: map[string]any{
+			"tags": []string{"code"},
+		},
+		SortKey:   "",
+		OrderDesc: false,
+		Intersect: false,
+	})
+	if err != nil {
+		t.Fatalf("Request failed: %s", err)
+	}
+	if resp == nil {
+		t.Fatalf("Didn't find file")
+	}
+	expectedPrevFileName := "2024-01-14.md"
+	if resp.PrevFileName != expectedPrevFileName {
+		t.Fail()
+		t.Logf("Prev file name not '%s'. Found: %s", expectedPrevFileName, resp.PrevFileName)
+	}
+	expectedNextFileName := "aoc23day4.md"
+	if resp.NextFileName != expectedNextFileName {
+		t.Fail()
+		t.Logf("Next file name not '%s'. Found: %s", expectedNextFileName, resp.NextFileName)
+	}
+}
